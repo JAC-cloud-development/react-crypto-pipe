bb4aa9f706361245769d7d01e8cab777185f7355
Marco Carrara <marco.carrara@runelab.it>
2022-03-16 00:27:59 +0100

    feat(naming): add some text

--- src/components/coinRow.js
+++ src/components/coinRow.js
@@ -1,29 +1,29 @@
-import {useEffect, useMemo, useState} from 'react';
-import {getCoinDetail, getCoinList} from '../api';
+import { useEffect, useMemo, useState } from 'react';
+import { getCoinDetail, getCoinList } from '../api';
 
-export const CoinRow = ({coin}) => {
+export const CoinRow = ({ coin }) => {
 
   const [open, setOpen] = useState(false)
 
   const onClickRow = async (coin) => {
-    console.log({selectedCoin: coin})
+    console.log({ selectedCoin: coin })
     const coinDetail = await getCoinDetail(coin.id)
   }
 
   return (
     <div key={coin.id}
-         className={'coin-row'}
-         onClick={() => onClickRow(coin)}
-         onMouseEnter={() => setOpen(true)}
-         onMouseLeave={() => setOpen(false)}
+      className={'coin-row'}
+      onClick={() => onClickRow(coin)}
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
     >
       <div className="coin-row-header">
-        <img src={coin.image.small}/>
-        <div>{coin.name}</div>
-        <div>{coin.symbol}</div>
+        <img src={coin.image.small} />
+        <div>name: {coin.name}</div>
+        <div>symbol: {coin.symbol}</div>
       </div>
 
-      {open && <div style={{fontSize: 16}}>
+      {open && <div style={{ fontSize: 16 }}>
         {coin.market_data.circulating_supply} Pezzi
       </div>}
     </div>
